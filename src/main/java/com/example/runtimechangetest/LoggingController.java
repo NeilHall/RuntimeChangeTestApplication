7d93a36abc88aa8460935ffdcd5d89db2f117306
Neil Hall <neil@thingsyettobe.co.uk>
2023-04-13 02:17:47 +0100

    configuration properties added

--- src/main/java/com/example/runtimechangetest/LoggingController.java
+++ src/main/java/com/example/runtimechangetest/LoggingController.java
@@ -2,7 +2,6 @@ package com.example.runtimechangetest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
